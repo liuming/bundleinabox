0f3d5e566ab02efbbaa2043c4e0853577fb1ad1e
Ming Liu <liuming@lmws.net>
2013-08-29 15:58:28 -0700

    move requires into bundleinabox.rb

--- lib/bundleinabox.rb
+++ lib/bundleinabox.rb
@@ -1,3 +1,13 @@
+# stdlib
+require 'fileutils'
+# dependencies
+require 'bundler'
+require 'bundler/cli'
+require 'bundler/friendly_errors'
+require 'geminabox'
+require 'geminabox_client'
+require 'rubygems/commands/inabox_command'
+# bundleinabox
 require "bundleinabox/version"
 
 module Bundleinabox
